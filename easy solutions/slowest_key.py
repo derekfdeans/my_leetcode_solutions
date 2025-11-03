0ada82f22fcd13509daa89902fb785b9885119f8
Derek Deans <71570230+derekfdeans@users.noreply.github.com>
2025-11-02 19:29:06 -0500

    Add slowestKey function to determine longest keypress
    Implement solution to find the slowest key pressed based on release times.

---
+++ easy solutions/slowest_key.py
@@ -0,0 +1,22 @@
+# A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+
+# You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence, and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released. Both arrays are 0-indexed. The 0th key was pressed at the time 0, and every subsequent key was pressed at the exact time the previous key was released.
+
+# The tester wants to know the key of the keypress that had the longest duration. The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1], and the 0th keypress had a duration of releaseTimes[0].
+
+# Note that the same key could have been pressed multiple times during the test, and these multiple presses of the same key may not have had the same duration.
+
+# Return the key of the keypress that had the longest duration. If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        slowest_time = releaseTimes[0]
+        slowest_key = keysPressed[0]
+
+        for index in range(1, len(releaseTimes)):
+            current_time = releaseTimes[index] - releaseTimes[index-1]
+            if slowest_time < current_time or slowest_time == current_time and slowest_key < keysPressed[index]:
+                    slowest_time = current_time
+                    slowest_key = keysPressed[index]
+
+        return slowest_key
